535858f44f818514dbf00d88cba17aa34b039d53
Sultan Iman <354868+imanhodjaev@users.noreply.github.com>
2019-10-11 17:51:17 +0200

    Update schema-macro.ex

--- ecto/schema-macro.ex
+++ ecto/schema-macro.ex
@@ -1,1 +1,22 @@
+defmodule MyApp.Base.Model do
+  defmacro __using__(_) do
+    quote do
+      use Ecto.Schema
 
+      import Ecto.Changeset
+      import Ecto.Query
+
+      # Set all datetimes to UTC by default
+      @timestamp_opts [type: :utc_datetime]
+    end
+  end
+end
+
+# Usage
+defmodule XYZ do
+  use MyApp.Base.Model
+  
+  schema "xyz" do
+    # ...
+  end
+end
