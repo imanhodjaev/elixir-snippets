1abd82a0f3b6f9a0d4252e9b9c711d2a100d3c51
Sultan Iman <354868+imanhodjaev@users.noreply.github.com>
2019-10-11 17:53:33 +0200

    Update query-macro.ex

--- ecto/query-macro.ex
+++ ecto/query-macro.ex
@@ -1,1 +1,16 @@
+defmodule MyApp.Base.Query do
+  defmacro __using__(_) do
+    quote do
+      import Ecto.Query, warn: false
+      alias MyApp.MyContext.{ModelOne, ModelTwo}
+    end
+  end
+end
 
+# Usage
+
+defmodule ModelXRepository do
+  use MyApp.Base.Query
+  
+  def all_model_on, do: ...
+end
